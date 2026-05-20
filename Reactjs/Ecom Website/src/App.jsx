ce918111a0d15e5f931e76cdbc919666652a61dd
Nikki Wong <thatkumo@gmail.com>
2026-05-20 16:58:08 +0100

    added auth

--- Reactjs/Ecom Website/src/App.jsx
+++ Reactjs/Ecom Website/src/App.jsx
@@ -8,16 +8,19 @@ import Home from "./pages/Home";
 import Checkout from "./pages/Checkout"
 import Auth from "./pages/Auth";
 import Navbar from "./components/Navbar";
+import AuthProvider from './context/AuthContext'
 
 function App() {
   return (
     <div className='app'>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/auth" element={<Auth/>} />
-        <Route path="/checkout" element={<Checkout/>} />
-      </Routes>
+      <AuthProvider>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </AuthProvider>
     </div>
   )
 }
